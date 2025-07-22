f11149c9f35cc469bfdf2f52b906dbac3649e59f
portify999 <portiportfoy@gmail.com>
2025-07-22 13:43:58 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -23,8 +23,8 @@ API_URL   = "https://www.kap.org.tr/tr/api/disclosure/members/byCriteria"
 
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 465
-MAIL_TO = ["ysflkrx@gmail.com"]
-MAIL_CC = [""]
+MAIL_TO = "ysflkrx@gmail.com"
+MAIL_CC = ""
 
 MAIL_USER = os.getenv("MAIL_USER")
 MAIL_PASS = os.getenv("MAIL_PASS")
