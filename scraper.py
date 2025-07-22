b0935d262aea44f56d61b5c3c110ea8ef2f118a2
portify999 <portiportfoy@gmail.com>
2025-07-22 11:25:22 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,8 +16,8 @@ INDEX_OID = "4028328c7bf4b5e4017d149764890f47"   # XK100
 
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 465
-MAIL_TO   = "investment@ktportfoy.com.tr"
-MAIL_CC   = "bayram.salur@ktportfoy.com.tr"
+MAIL_TO   = "ysflkrx@gmail.com"
+MAIL_CC   = ""
 MAIL_SUBJ = f"Günlük KAP Bildirim Raporu {FROM_DATE} - {TO_DATE}"
 
 MAIL_USER = os.getenv("MAIL_USER")
